cd34d2aded6a18149206e5bc6e0a7a521e903926
kangddong <rkdehddud96@naver.com>
2024-10-11 20:45:16 +0900

    3. 데이터 소스 설정하기 4. 스냅샷 설정하기

--- Getting started with Today/DetailViewController/ReminderViewController.swift
+++ Getting started with Today/DetailViewController/ReminderViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ReminderViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Row>
+    
     var reminder: Reminder
+    var dataSource: DataSource!
     
     init(reminder: Reminder) {
         self.reminder = reminder
@@ -22,6 +26,32 @@ class ReminderViewController: UICollectionViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // CellRegistration은 셀의 내용과 모양을 구성하는 방법을 지정합니다.
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, itemIdentifier: Row) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
+        
+        updateSnapshot()
+        
+        collectionView.dataSource = dataSource
+    }
+    
+    func cellRegistrationHandler(
+        cell: UICollectionViewListCell, indexPath: IndexPath, row: Row
+    ) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        cell.contentConfiguration = contentConfiguration
+        cell.tintColor = .todayPrimaryTint
+    }
+    
     func text(for row: Row) -> String? {
         switch row {
         case .date: return reminder.dueDate.dayText
@@ -31,9 +61,11 @@ class ReminderViewController: UICollectionViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    func updateSnapshot() {
+        var snapshot = Snapshot()
+        snapshot.appendSections([0]) // 섹션 추가하기
+        // 아이템 추가하기
+        snapshot.appendItems([Row.title, Row.date, Row.time, Row.notes], toSection: 0)
+        dataSource.apply(snapshot)
     }
 }
