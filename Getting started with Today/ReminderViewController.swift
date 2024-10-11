8c23d4b452a858573d509100449fca62fba21eac
kangddong <rkdehddud96@naver.com>
2024-10-11 20:28:41 +0900

    1. 리마인더 뷰 만들기

---
+++ Getting started with Today/ReminderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReminderViewController.swift
+//  Getting started with Today
+//
+//  Created by 강동영 on 10/11/24.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        super.init(collectionViewLayout: listLayout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
