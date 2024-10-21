83df46774a4e8443a4fa439caade82b86dc72e3d
kangddong <rkdehddud96@naver.com>
2024-10-21 23:22:00 +0900

    3. 모델 유형 간에 변환

---
+++ Getting started with Today/Models/EKReminder+Reminder.swift
@@ -0,0 +1,29 @@
+//
+//  EKReminder+Reminder.swift
+//  Getting started with Today
+//
+//  Created by 강동영 on 10/21/24.
+//
+
+import EventKit
+import Foundation
+
+extension EKReminder {
+    func update(using reminder: Reminder, in store: EKEventStore) {
+        title = reminder.title
+        notes = reminder.notes
+        isCompleted = reminder.isComplete
+        calendar = store.defaultCalendarForNewReminders()
+        alarms?.forEach { alarm in
+            guard let absoluteDate = alarm.absoluteDate else { return }
+            let comparison = Locale.current.calendar.compare(
+                reminder.dueDate, to: absoluteDate, toGranularity: .minute)
+            if comparison != .orderedSame {
+                removeAlarm(alarm)
+            }
+        }
+        if !hasAlarms {
+            addAlarm(EKAlarm(absoluteDate: reminder.dueDate))
+        }
+    }
+}
